a5c4c5d62dd297986707ef8881c0fcd657c44d82
reyna510 <reynacabrera510@gmail.com>
2017-05-23 23:26:59 +0000

    added devise and login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #added as per devise installation instructions
+   config.action_mailer.default_url_options = { host:'new-rubypractice-13-2-reyna510.c9users.io/' }
 end
